9ef4bbdc01cd9feb8793c2f61d6cf8d0a988325a
akageleiry <akageleiry@gmail.com>
2016-05-14 16:28:34 -0700

    Add EnsurePackages function

---
+++ EnsurePackages.R
@@ -0,0 +1,32 @@
+
+EnsurePackages <- function(pkg) {
+  
+  #
+  # Author:  Andy Kageleiry
+  # Updated: 2016-05-14
+  #
+  # Description:
+  # Short function to (a) ensure a single package is installed then (b) load it.
+  #
+  # Inputs:
+  # pkg = Package name, in quotes
+  #
+  # Outputs:
+  # [none]
+  #
+  # Disclaimer:
+  # This material is for private use only. Any use (including but not limited
+  # to copying, modifying, or redistributing) for either personal or
+  # commercial use without express permission from the author is strictly
+  # prohibited.
+  #
+  
+  # Install if the package is not yet installed
+  if (!(pkg %in% installed.packages()[,1])) {
+    install.packages(pkg)
+  }
+  
+  # Load package
+  require(pkg, character.only = T)
+  
+}
